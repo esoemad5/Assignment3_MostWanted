f091f991a2697ab7d2835679ffeef83a70f373a3
esoemad5 <esoemad5@gmail.com>
2018-09-14 14:29:29 -0500

    To avoid conflicts, Ive made a new .js file 'elliottsFunctions.js' for me to work in. Once a function works, I'll add it to 'app.js' and push it.

---
+++ elliottsFunctions.js
@@ -0,0 +1,24 @@
+/*
+ * Will slowly move these functions into app.js so we all aren't working on the same file.
+ */
+ 
+ 				
+				message += person.firstName + " " + person.lastName + "'s parents: ";
+				for(let i = 0; i < person.parents.length; i++){
+					// TODO: Search data.js for  people by id.
+					// filter out all non-parents, get an array
+					personsParents = person.parents.filter(function(el){
+						for(let j = 0; j < people.length; j++){
+							if(people[j].id == el){// If the person in data.js has an id that is an id in the found person's parents array.
+								return true;
+							}
+						}
+					});
+				}
+				
+				// different approach
+				personsParents = people.filter(function(el){
+					for(let i = 0; i < person.parents.length; i++){
+						
+					}
+				});
