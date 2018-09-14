a51d368d1dc9078777f8aed2b68a034d4eca112b
esoemad5 <esoemad5@gmail.com>
2018-09-14 11:40:03 -0500

    Working in mainMenu. Changed tabbing. Hopefully no confilict.

--- app.js
+++ app.js
@@ -64,10 +64,10 @@ function searchByTraits(people) {
 }
 
 function searchByWeight(people) {
-  let userInputWeight = prompt("How much does the person weigh?");
+	let userInputWeight = prompt("How much does the person weigh?");
 
-  let newArray = people.filter(function (el) {
-    if(el.weight == userInputWeight) {
+	let newArray = people.filter(function (el) {
+	if(el.weight == userInputWeight) {
       return true;
     }
     // return true if el.weight matches userInputHeight
@@ -79,33 +79,33 @@ function searchByWeight(people) {
 // Menu function to call once you find who you are looking for
 function mainMenu(person, people){
 
-  /* Here we pass in the entire person object that we found in our search, as well as the entire original dataset of people. We need people in order to find descendants and other information that the user may want. */
+	/* Here we pass in the entire person object that we found in our search, as well as the entire original dataset of people. We need people in order to find descendants and other information that the user may want. */
 
-  if(!person){
-    alert("Could not find that individual.");
-    return app(people); // restart
-  }
+	if(!person){
+		alert("Could not find that individual.");
+		return app(people); // restart
+	}
 
-  var displayOption = prompt("Found " + person.firstName + " " + person.lastName + " . Do you want to know their 'info', 'family', or 'descendants'? Type the option you want or 'restart' or 'quit'");
+	var displayOption = prompt("Found " + person.firstName + " " + person.lastName + " . Do you want to know their 'info', 'family', or 'descendants'? Type the option you want or 'restart' or 'quit'");
 
-  switch(displayOption){
-    case "info":
-    // TODO: get person's info
-    break;
-    case "family":
-    // TODO: get person's family
-    break;
-    case "descendants":
-    // TODO: get person's descendants
-    break;
-    case "restart":
-    app(people); // restart
-    break;
-    case "quit":
-    return; // stop execution
-    default:
-    return mainMenu(person, people); // ask again
-  }
+	switch(displayOption){
+		case "info":
+			// TODO: get person's info
+			break;
+		case "family":
+			// TODO: get person's family
+			break;
+		case "descendants":
+			// TODO: get person's descendants
+			break;
+		case "restart":
+			app(people); // restart
+			break;
+		case "quit":
+			return; // stop execution
+		default:
+			return mainMenu(person, people); // ask again
+	}
 }
 
 function searchByName(people){
