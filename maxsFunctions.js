28e075e32d1103e7afb0159784e72bcbbeade1e5
imilian007 <maxketter01@gmail.com>
2018-09-17 09:47:22 -0500

    new function for displaying people

--- maxsFunctions.js
+++ maxsFunctions.js
@@ -1,21 +1,7 @@
-function displayInTable(person){
-	document.getElementById("displayTablePersonName").innerHTML = person.firstName + person.lastName;
-
-	document.getElementById("displayGender").innerHTML = person.gender;
-
-	document.getElementById("displayAge").innerHTML = person.age;
-
-	document.getElementById("displayHeight").innerHTML = person.height;
-
-	document.getElementById("displayWeight").innerHTML = person.weight;
-
-	document.getElementById("displayEyeColor").innerHTML = person.eyeColor;
-
-	document.getElementById("displayOccupation").innerHTML = person.occupation;
-
-	document.getElementById("displayParents").innerHTML = person.parents;
-
-	document.getElementById("displayCurrentSpouse").innerHTML = person.currentSpouse;
-
-	document.getElementById("displayChildren").innerHTML = person.children;
+function displayInTable(people){
+	let output = "";
+	for(let i = 0; i < people.length; i++){
+		output += "<tr><td>";
+		output +=
+	}
 }
