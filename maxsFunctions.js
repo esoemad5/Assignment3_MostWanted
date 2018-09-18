3c2a1f7475848670176a867f46d605d31d237bf4
imilian007 <maxketter01@gmail.com>
2018-09-18 09:54:05 -0500

    css styling and formatting

--- maxsFunctions.js
+++ maxsFunctions.js
@@ -8,7 +8,7 @@ function displayInTable(peopleArray, showRelationshipToElement0){
 		output += "<tr><th>Select this person</th><th>Name</th><th>Gender</th><th>Age</th><th>Height</th><th>Weight</th><th>Eye Color</th><th>Occupation</th></tr>";
 	}
 	for(let i = 0; i < peopleArray.length; i++){
-		output += '<tr><td><button onClick="mainMenu(currentArray['+i+'])">Select this person</button> &nbsp </td><td>'; // There is a possible issue with this line if the rest of the code is modified. Check here if there is an issue with the people butttons.
+		output += '<tr><td><button onClick="mainMenu(currentArray['+i+'])" class="btn btn-outline-success">Select this person</button> &nbsp </td><td>'; // There is a possible issue with this line if the rest of the code is modified. Check here if there is an issue with the people butttons.
 		if(showRelationshipToElement0){ output += peopleArray[i].relationship; output += "</td><td>" }
 		output += peopleArray[i].firstName + " ";
 		output += peopleArray[i].lastName;
