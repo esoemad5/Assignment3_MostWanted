9d1fb6ffdddee4956f5ad621878877310bf679cb
esoemad5 <esoemad5@gmail.com>
2018-09-18 10:03:54 -0500

    Moved 'displayInTable' from 'maxsFunctions' to 'testApp'. 'elliottsFunctions', 'maxsFunctions', and 'app' are all obsolete. Should delete to remove clutter. Updated TODO.txt for the first time in a while. Validation is fine; could be better.

--- testApp.js
+++ testApp.js
@@ -120,6 +120,44 @@ function searchButton(){
 	//console.log(currentArray);
 }
 
+function displayInTable(peopleArray, showRelationshipToElement0){ // Returns the html script to show search results.
+	let output = ""
+	if(showRelationshipToElement0){
+		output += "<tr><th>Select this person</th><th>Relationship to Selection</th><th>Name</th><th>Gender</th><th>Age</th><th>Height</th><th>Weight</th><th>Eye Color</th><th>Occupation</th></tr>";
+
+	}
+	else{ 	
+		output += "<tr><th>Select this person</th><th>Name</th><th>Gender</th><th>Age</th><th>Height</th><th>Weight</th><th>Eye Color</th><th>Occupation</th></tr>";
+	}
+	for(let i = 0; i < peopleArray.length; i++){
+		output += '<tr><td><button onClick="mainMenu(currentArray['+i+'])" class="btn btn-outline-success">Select this person</button> &nbsp </td><td>'; // There is a possible issue with this line if the rest of the code is modified. Check here if there is an issue with the people butttons.
+		if(showRelationshipToElement0){ output += peopleArray[i].relationship; output += "</td><td>" }
+		output += peopleArray[i].firstName + " ";
+		output += peopleArray[i].lastName;
+		output += "</td>";
+		output += "<td>";
+		output += peopleArray[i].gender;
+		output += "</td>";
+		output += "<td>";
+		output += peopleArray[i].age;
+		output += "</td>";
+		output += "<td>";
+		output += peopleArray[i].height;
+		output += "</td>";
+		output += "<td>";
+		output += peopleArray[i].weight;
+		output += "</td>";
+		output += "<td>";
+		output += peopleArray[i].eyeColor;
+		output += "</td>";
+		output += "<td>";
+		output += peopleArray[i].occupation;
+		output += "</td></tr>";
+	}
+
+	return output;
+}
+
  /*
   * Returns a people array of the person's spouse(s). Changed output from an object to an array because of a bug. Turns out, I was just spelling a variable wrong. On the bright side, it can handle pollygamy.
   */
