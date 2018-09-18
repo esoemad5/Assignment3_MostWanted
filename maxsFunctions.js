3f187a1c6ed8e55a6e867935da7092872f8a97b4
imilian007 <maxketter01@gmail.com>
2018-09-18 08:42:49 -0500

    resolution

--- maxsFunctions.js
+++ maxsFunctions.js
@@ -4,10 +4,9 @@ function displayInTable(peopleArray, showRelationshipToElement0){
 		output += "<tr><th>Select this person</th><th>Relationship to Selection</th><th>Name</th><th>Gender</th><th>Age</th><th>Height</th><th>Weight</th><th>Eye Color</th><th>Occupation</th></tr>";
 
 	}
-	else{
-		
+	else{ 	
 		output += "<tr><th>Select this person</th><th>Name</th><th>Gender</th><th>Age</th><th>Height</th><th>Weight</th><th>Eye Color</th><th>Occupation</th></tr>";
-		
+	}
 	for(let i = 0; i < peopleArray.length; i++){
 		output += '<tr><td><button onClick="mainMenu(currentArray['+i+'])">Select this person</button> &nbsp </td><td>'; // There is a possible issue with this line if the rest of the code is modified. Check here if there is an issue with the people butttons.
 		if(showRelationshipToElement0){ output += peopleArray[i].relationship; output += "</td><td>" }
